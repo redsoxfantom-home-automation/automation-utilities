ec7346c56c10fe66dfd8420dee1c30f1b63cd954
Tom English <redsoxfantom@gmail.com>
2017-02-20 21:18:27 -0500

    Added logging to zookeeper connection

--- python_utils/python_utils/zookeeper.py
+++ python_utils/python_utils/zookeeper.py
@@ -2,20 +2,27 @@ from kazoo.client import KazooClient
 import os
 import socket
 import json
+import logging
 
 class ServiceAccessor(object):
    def __init__(self):
+      logging.info("Initializing service accessor")
       root_path = os.path.realpath(os.path.dirname(__file__))
       config_location = os.path.join(root_path,"config.json")
       config_data = json.load(open(config_location))
+
       self.zkhost = config_data['zk_host']
-      self.port = config_data['zk_port']
+      self.port = str(config_data['zk_port'])
+      logging.info("Creating zookeeper client to connect to instance at %s:%s" % (self.zkhost,self.port))
       self.hostname = socket.gethostname()
       self.root_name = "services"
-      self.zk = KazooClient(hosts='%s:%s' % (self.zkhost,self.port))
+      self.zk = KazooClient(hosts="%s:%s" % (self.zkhost,self.port))
+      logging.info("Successfully initialized")
 
    def connect(self):
+      logging.info("Attempting to connect to zookeeper instance...")
       self.zk.start()
+      logging.info("Successfully connected")
 
    def register_service(self, api_level, service_name, port):
       self.api_level = api_level
@@ -25,6 +32,8 @@ class ServiceAccessor(object):
       root_path = "%s/%s/%s" % (self.root_name,api_level,service_name)
       host_name_path = "%s/host" % root_path
       port_path = "%s/port" % root_path
+
+      logging.info("Registering %s service (version %s) to %s:%s" % (service_name,api_level,self.hostname,port))
 
       self.__create_node(host_name_path,self.hostname)
       self.__create_node(port_path,port)
